7696e5dce433f324a908c567f36bfd6e5b060a69
rephlex00 <20785249+rephlex00@users.noreply.github.com>
2025-11-05 19:50:00 -0500

    Fix subdirectory image linking

--- patch.sh
+++ patch.sh
@@ -7,3 +7,4 @@ PATCH_DIR=".venv/lib/python3.11/site-packages"
 patch -p1 -d "$PATCH_DIR" < supernotelib-ratta-fallback.patch
 patch -p1 -d "$PATCH_DIR" < sn2md-year-month-day.patch
 patch -p1 -d "$PATCH_DIR" < sn2md-metadata-dotmeta.patch
+patch -p1 -d "$PATCH_DIR" < sn2md-images-subdir.patch
